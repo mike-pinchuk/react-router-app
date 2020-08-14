89c8b6106933e948befd968bda55e3a4a3b34f2b
Mike <morning.sunshine.work@gmail.com>
2020-08-14 20:24:43 +0300

    Updates

---
+++ precache-manifest.7248a7b9549c00061d861da1b684c267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f32186a7644b4ba84c4e98fbb9c6bee",
+    "url": "/github.io/react-router-app/index.html"
+  },
+  {
+    "revision": "3a4a981e5bc90560e430",
+    "url": "/github.io/react-router-app/static/css/main.88b4d9e2.chunk.css"
+  },
+  {
+    "revision": "7d20fb1abe701cd25228",
+    "url": "/github.io/react-router-app/static/js/2.2860f24e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/react-router-app/static/js/2.2860f24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4a981e5bc90560e430",
+    "url": "/github.io/react-router-app/static/js/main.bced705e.chunk.js"
+  },
+  {
+    "revision": "06febe58bdb11279fc72",
+    "url": "/github.io/react-router-app/static/js/runtime-main.6f935506.js"
+  }
+]);
